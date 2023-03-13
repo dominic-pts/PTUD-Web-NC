0dab7fa32e0d05589868718933ccaf7310367370
Pham Son <phamthanhson99999999@gmail.com>
2023-03-13 08:26:04 +0700

    chuyen nhanh

--- src/TipsAndTricks/TatBlog.WebApp/Extensions/RouteExtensions.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Extensions/RouteExtensions.cs
@@ -31,7 +31,22 @@
                 pattern: "{controller=Blog}/{action=Index}/{id?}"
               );
 
-            return endpoints;
+			endpoints.MapControllerRoute(
+				name: "single-post",
+				pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
+                defaults: new { controller = "Blog", action = "Post" }
+			  );
+			endpoints.MapControllerRoute(
+				name: "admin-area",
+				pattern: "admin/{controller=Dashboard}/{action=Index}/{id?}",
+				defaults: new { area= "Admin" }
+			  );
+			endpoints.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Blog}/{action=Index}/{id?}"
+			  );
+
+			return endpoints;
         }
     }
 }
